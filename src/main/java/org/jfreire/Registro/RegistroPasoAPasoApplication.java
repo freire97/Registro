6de1e23a6d1ed89d3307436e0cc521a5a392ba4c
Jorge <jorgefreirefdez@gmail.com>
2022-03-17 13:02:57 +0100

    Commit inicial pendiente de pojos

---
+++ src/main/java/org/jfreire/Registro/RegistroPasoAPasoApplication.java
@@ -0,0 +1,13 @@
+package org.jfreire.Registro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroPasoAPasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroPasoAPasoApplication.class, args);
+	}
+
+}
